b82fcadf5d55eff781eb78e082359deb47d23d05
Joelma Camargo <joelmavilela023@gmail.com>
2023-10-11 21:19:50 -0300

    Atribuindo valores para bool e char

---
+++ TiposDeDados3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Bool e Char
+Console.WriteLine("Atribuindo valores para bool e char");
+
+bool ativo = true;
+Boolean inativo = false;
+
+Console.WriteLine(ativo);
+Console.WriteLine(inativo);
+
+Console.WriteLine( 10 == 15);
+Console.WriteLine( 10 == 10);
+
+int x = 10;
+int y = 15;
+Console.WriteLine(x > y);
+
+//Definir uma variável do tipo char
+char letra1 = 'A';
+char letra2 = '\u0041'; //outra representação do caracter é fazer no formato unicode(padrão atual usado para codificação de texto universal e engloba todos os idiomas. 
+
+Console.WriteLine(letra1);
+Console.WriteLine(letra2);
+
+Console.ReadLine();
+
