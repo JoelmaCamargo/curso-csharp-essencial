ceb3d851964cdf050463723c65fe71014f83ed3a
Joelma Camargo <joelmavilela023@gmail.com>
2023-10-11 21:18:08 -0300

    Declarando variavéis numéricas

---
+++ TiposDeDados1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Tipos de dados
+Console.WriteLine("Declarando variáveis numéricas");
+Console.WriteLine();
+//declarando variáveis
+byte valor1 = 255;
+sbyte valor2 = -127;
+int valor3 = -2147483647;
+valor3 = 1000; //para variavéis eu posso alterar o valor atribuído, constante não.
+uint valor4 = 2147483647;
+long valor5 = -21474836489;
+
+const int valor6 = 999;
+//valor6 = 888; não permite alterar o valor da constante
+
+int var1 = 8, var2 = 9, var3 = 10;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+Console.WriteLine(valor6);
+Console.WriteLine(var1);
+Console.WriteLine(var2);
+Console.WriteLine(var3);
+
+Console.ReadLine();
