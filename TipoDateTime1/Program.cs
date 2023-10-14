13648daa80fcaef91163e9680e291d824afbfe98
Joelma Camargo <joelmavilela023@gmail.com>
2023-10-14 20:04:40 -0300

    Principais propriedades e métodos para trabalhar com datas e horas

---
+++ TipoDateTime1/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("## Struct DateTime ##\n");
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+
+//criar uma data específica usa formato: aaaa, mm, dd
+DateTime dataHoje = new DateTime(2022, 09, 06);
+Console.WriteLine(dataHoje);
+
+//definir data e as horas específica
+DateTime dataHoraHoje = new DateTime(2022, 09, 06, 21, 10, 30);
+Console.WriteLine(dataHoraHoje);
+
+DateTime hoje = DateTime.Now;
+
+//extrair informações da data atual
+Console.WriteLine(hoje.Year);
+Console.WriteLine(hoje.Month);
+Console.WriteLine(hoje.Day);
+Console.WriteLine(hoje.Hour);
+Console.WriteLine(hoje.Minute);
+Console.WriteLine(hoje.Second);
+Console.WriteLine(hoje.Millisecond);
+
+//adiconando valores
+Console.WriteLine(hoje.AddDays(30));
+Console.WriteLine(hoje.AddMonths(1));
+Console.WriteLine(hoje.AddHours(2));
+Console.WriteLine(hoje.AddYears(5));
+
+//obter o dia da semana e do ano
+Console.WriteLine(hoje.DayOfWeek);
+Console.WriteLine(hoje.DayOfYear);
+
+//data no formato longo e curto
+Console.WriteLine(hoje.ToLongDateString());
+Console.WriteLine(hoje.ToShortDateString());
+
+
+Console.WriteLine(hoje.ToLongTimeString());
+Console.WriteLine(hoje.ToShortTimeString());
+
+Console.ReadLine();
+
