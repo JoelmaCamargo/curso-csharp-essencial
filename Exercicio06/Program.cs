7b4396fcb81d055adba0127ba0fc15f252d012e5
Joelma Camargo <joelmavilela023@gmail.com>
2023-10-20 17:58:55 -0300

    Exercícios 6 e 7

---
+++ Exercicio06/Program.cs
@@ -0,0 +1,7 @@
+﻿// Exercício 06 = Declare uma variável nota do tipo double como sendo do tipo Nullable type
+// e atribua o valor 7.80 e exiba o seu valor.
+
+double? nota = 7.80;
+Console.WriteLine(nota);
+
+Console.ReadLine();
---
+++ Exercicio07/Program.cs
@@ -0,0 +1,2 @@
+﻿// Exercício 07 = Quais as diferenças entre os tipos por valor e os tipos por referência
+Console.WriteLine("Hello, World!");
